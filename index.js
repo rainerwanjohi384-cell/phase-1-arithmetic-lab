1f241541a7ddd85ddc1c7b5f422208168f67bbdd
rainerwanjohi384-cell <rainerwanjohi384@gmail.com>
2025-09-20 18:13:22 +0300

    complete JS arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; // 31 * 2 = 62
+
+let random = Math.floor(Math.random() * 100) + 1; // Range: 1 to 100
+
+let num3 = 20;
+let num4 = 8;
+let mod = num3 % num4; // 20 % 8 = 4
+
+let max = Math.max(1, 5, 10, 15, 20);
